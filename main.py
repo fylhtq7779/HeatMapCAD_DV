9047f9b46bdb8056cf4786933e58e13774020ecb
fylhtq7779 <fy.82@bk.ru>
2024-10-16 15:26:17 +0300

    new func

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import ttk, filedialog, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import time
 import json
 import os
 import sys
 import ttkbootstrap as ttkb
-from matplotlib.colors import Normalize
+
 
 class MouseTrackerApp:
     def __init__(self, root):
@@ -20,20 +20,20 @@ class MouseTrackerApp:
         self.root.geometry("1200x800")
         self.root.minsize(width=1200, height=800)
 
-        # Параметры для тепловой карты и режима временной карты
         self.positions = []
         self.listener = None
-        self.color_map = 'hot'
+        self.color_map_moving = 'hot'
+        self.color_map_clicking = 'cool'
         self.img = None
         self.screen_resolution = None
         self.tracks_directory = "mouse_tracks"
-        
+        self.is_click_mode = tk.BooleanVar(value=False)
+        self.is_move_mode = tk.BooleanVar(value=True)  # Включено по умолчанию
+
         if not os.path.exists(self.tracks_directory):
             os.makedirs(self.tracks_directory)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
-
-        self.is_time_map = tk.BooleanVar(value=False)  # Переменная для чекбокса
 
         self.setup_ui()
         self.update_tracks_combobox()
@@ -56,13 +56,18 @@ class MouseTrackerApp:
         self.canvas_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
         button_style = {'bootstyle': 'primary', 'padding': 10, 'takefocus': 0}
-        slider_style = {'bootstyle': 'success', 'orient': tk.HORIZONTAL, 'takefocus': 0}
 
         ttkb.Button(settings_frame, text="Начать отслеживание", command=self.start_tracking, **button_style).pack(
             anchor=tk.N, fill=tk.X, pady=10)
         ttkb.Button(settings_frame, text="Завершить отслеживание", command=self.stop_tracking, **button_style).pack(
             anchor=tk.N, fill=tk.X, pady=10)
-        
+
+        ttkb.Checkbutton(settings_frame, text="Карта нажатий", variable=self.is_click_mode, command=self.update_heatmap,
+                         bootstyle='round-toggle').pack(anchor=tk.N, pady=10)
+
+        ttkb.Checkbutton(settings_frame, text="Карта перемещений", variable=self.is_move_mode, command=self.update_heatmap,
+                         bootstyle='round-toggle').pack(anchor=tk.N, pady=10)
+
         ttkb.Label(settings_frame, text="Выберите маршрут", font=("Arial", 10)).pack(anchor=tk.N)
         self.track_combobox = ttkb.Combobox(settings_frame, state='readonly', takefocus=0)
         self.track_combobox.pack(anchor=tk.N, fill=tk.X, pady=10)
@@ -72,48 +77,60 @@ class MouseTrackerApp:
             anchor=tk.N, fill=tk.X, pady=10)
 
         ttkb.Label(settings_frame, text="Разрешение", font=("Arial", 10)).pack(anchor=tk.N)
-        self.slider_resolution = ttkb.Scale(settings_frame, from_=10, to=500, **slider_style)
-        self.slider_resolution.set(100)
+        self.slider_resolution = ttkb.Scale(settings_frame, from_=10, to=500, bootstyle='success', orient=tk.HORIZONTAL,
+                                            takefocus=0)
+        self.slider_resolution.set(400)
         self.slider_resolution.pack(anchor=tk.N, fill=tk.X, pady=10)
 
         ttkb.Label(settings_frame, text="Яркость", font=("Arial", 10)).pack(anchor=tk.N)
-        self.slider_brightness = ttkb.Scale(settings_frame, from_=0.1, to=3.0, **slider_style)
+        self.slider_brightness = ttkb.Scale(settings_frame, from_=0.1, to=3.0, bootstyle='success',
+                                            orient=tk.HORIZONTAL, takefocus=0)
         self.slider_brightness.set(1.0)
         self.slider_brightness.pack(anchor=tk.N, fill=tk.X, pady=10)
 
         ttkb.Label(settings_frame, text="Размер", font=("Arial", 10)).pack(anchor=tk.N)
-        self.slider_size = ttkb.Scale(settings_frame, from_=0.1, to=3.0, **slider_style)
+        self.slider_size = ttkb.Scale(settings_frame, from_=0.1, to=3.0, bootstyle='success', orient=tk.HORIZONTAL,
+                                      takefocus=0)
         self.slider_size.set(1.0)
         self.slider_size.pack(anchor=tk.N, fill=tk.X, pady=10)
 
         ttkb.Label(settings_frame, text="Чувствительность", font=("Arial", 10)).pack(anchor=tk.N)
-        self.slider_sensitivity = ttkb.Scale(settings_frame, from_=0.1, to=5.0, **slider_style)
+        self.slider_sensitivity = ttkb.Scale(settings_frame, from_=0.1, to=5.0, bootstyle='success',
+                                             orient=tk.HORIZONTAL, takefocus=0)
         self.slider_sensitivity.set(1.0)
         self.slider_sensitivity.pack(anchor=tk.N, fill=tk.X, pady=10)
 
-        cmap_label = ttkb.Label(settings_frame, text="Цветовая карта", font=("Arial", 10))
-        cmap_label.pack(anchor=tk.N)
+        # Binding slider release event to update the heatmap
+        self.slider_resolution.bind("<ButtonRelease-1>", lambda event: self.update_heatmap())
+        self.slider_brightness.bind("<ButtonRelease-1>", lambda event: self.update_heatmap())
+        self.slider_size.bind("<ButtonRelease-1>", lambda event: self.update_heatmap())
+        self.slider_sensitivity.bind("<ButtonRelease-1>", lambda event: self.update_heatmap())
 
+        # Цветовая схема для перемещений
+        ttkb.Label(settings_frame, text="Цвет карты перемещений", font=("Arial", 10)).pack(anchor=tk.N)
         cmap_options = ['hot', 'cool', 'viridis', 'plasma', 'inferno', 'magma', 'cividis']
-        self.cmap_combobox = ttkb.Combobox(settings_frame, values=cmap_options, state='readonly', takefocus=0)
-        self.cmap_combobox.set(self.color_map)
-        self.cmap_combobox.pack(anchor=tk.N, fill=tk.X, pady=10)
-        self.cmap_combobox.bind("<<ComboboxSelected>>", self.change_color_map)
+        self.cmap_move_combobox = ttkb.Combobox(settings_frame, values=cmap_options, state='readonly', takefocus=0)
+        self.cmap_move_combobox.set(self.color_map_moving)
+        self.cmap_move_combobox.pack(anchor=tk.N, fill=tk.X, pady=10)
+        self.cmap_move_combobox.bind("<<ComboboxSelected>>", lambda event: self.update_move_color_map())
 
-        ttkb.Checkbutton(settings_frame, text="Временная карта", variable=self.is_time_map,
-                         command=self.update_map, takefocus=0).pack(anchor=tk.N, pady=10)
+        # Цветовая схема для нажатий
+        ttkb.Label(settings_frame, text="Цвет карты нажатий", font=("Arial", 10)).pack(anchor=tk.N)
+        self.cmap_click_combobox = ttkb.Combobox(settings_frame, values=cmap_options, state='readonly', takefocus=0)
+        self.cmap_click_combobox.set(self.color_map_clicking)
+        self.cmap_click_combobox.pack(anchor=tk.N, fill=tk.X, pady=10)
+        self.cmap_click_combobox.bind("<<ComboboxSelected>>", lambda event: self.update_click_color_map())
 
         ttkb.Button(settings_frame, text="Сохранить тепловую карту", command=self.save_heatmap, **button_style).pack(
             anchor=tk.S, fill=tk.X, pady=20)
 
-        self.slider_resolution.bind("<ButtonRelease-1>", lambda event: self.update_map())
-        self.slider_brightness.bind("<ButtonRelease-1>", lambda event: self.update_map())
-        self.slider_size.bind("<ButtonRelease-1>", lambda event: self.update_map())
-        self.slider_sensitivity.bind("<ButtonRelease-1>", lambda event: self.update_map())
+    def update_move_color_map(self):
+        self.color_map_moving = self.cmap_move_combobox.get()
+        self.update_heatmap()
 
-    def change_color_map(self, event):
-        self.color_map = self.cmap_combobox.get()
-        self.update_map()
+    def update_click_color_map(self):
+        self.color_map_clicking = self.cmap_click_combobox.get()
+        self.update_heatmap()
 
     def start_tracking(self):
         delay_seconds = 5
@@ -124,10 +141,10 @@ class MouseTrackerApp:
             self.screen_resolution = (self.img.shape[1], self.img.shape[0])
 
             self.positions.clear()
-            self.listener = mouse.Listener(on_move=self.on_move)
+            self.listener = mouse.Listener(on_move=self.on_move, on_click=self.on_click)
             self.listener.start()
 
-        messagebox.showinfo(
+        msg_box = messagebox.showinfo(
             "Информация",
             f"У вас есть {delay_seconds} секунд, чтобы свернуть окна и подготовить экран для скриншота..."
         )
@@ -135,18 +152,22 @@ class MouseTrackerApp:
         self.root.after(delay_seconds * 1000, start_after_delay)
 
     def on_move(self, x, y):
-        timestamp = time.time()
-        self.positions.append((x, y, timestamp))
+        self.positions.append((x, y, False))  # False означает, что это просто перемещение
+
+    def on_click(self, x, y, button, pressed):
+        if button == mouse.Button.left and pressed:
+            self.positions.append((x, y, True))  # True означает, что это клик
 
     def stop_tracking(self):
         if self.listener:
             self.listener.stop()
             self.listener.join()
             self.listener = None
-            self.update_map()
+            self.update_heatmap()
             filename = self.save_positions()
-            self.update_tracks_combobox()
-            self.track_combobox.set(os.path.basename(filename))
+            if filename:
+                self.update_tracks_combobox()
+                self.track_combobox.set(os.path.basename(filename))
 
     def save_positions(self):
         if self.positions and self.screen_resolution:
@@ -163,6 +184,7 @@ class MouseTrackerApp:
                 json.dump(data, f)
             messagebox.showinfo("Информация", f"Движения мыши сохранены в: {filename}")
             return filename
+        return ""
 
     def update_tracks_combobox(self):
         files = [f for f in os.listdir(self.tracks_directory) if f.endswith('.json')]
@@ -177,7 +199,7 @@ class MouseTrackerApp:
             self.positions = data['positions']
             res = data['resolution']
             self.img = np.zeros((res['height'], res['width'], 3), dtype=np.uint8)
-            self.update_map()
+            self.update_heatmap()
             messagebox.showinfo("Информация", f"Выбранный маршрут загружен: {selected_file}")
 
     def load_image(self):
@@ -185,16 +207,11 @@ class MouseTrackerApp:
             filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpeg"), ("All files", "*.*")])
         if filepath:
             self.img = np.array(plt.imread(filepath))
-            self.update_map()
+            self.update_heatmap()
             messagebox.showinfo("Информация", f"Изображение загружено: {filepath}")
 
-    def create_heatmap(self, resolution, brightness, size_factor, sensitivity, img, x, y):
-        if img is None:
-            img = np.zeros((800, 1200, 3), dtype=np.uint8)
-
-        self.ax.clear()  # Очистить график перед построением
-
-        heatmap, xedges, yedges = np.histogram2d(x, y, bins=[resolution, resolution],
+    def create_heatmap(self, resolution, brightness, size_factor, sensitivity, img, positions, cmap):
+        heatmap, xedges, yedges = np.histogram2d(*positions, bins=[resolution, resolution],
                                                  range=[[0, img.shape[1]], [0, img.shape[0]]])
         heatmap = heatmap ** sensitivity
         heatmap = gaussian_filter(heatmap, sigma=3 * size_factor)
@@ -206,83 +223,33 @@ class MouseTrackerApp:
         heatmap *= brightness
         heatmap = np.clip(heatmap, 0, 1)
 
-        self.ax.imshow(np.flipud(img))
-        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-        self.ax.imshow(heatmap.T, extent=extent, origin='upper', cmap=self.color_map, alpha=heatmap.T)
-        self.ax.axis('off')
-        self.fig.tight_layout()
-        self.canvas.draw()  # Использовать canvas.draw вместо fig.canvas.draw
+        self.ax.imshow(heatmap.T, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], origin='upper',
+                       cmap=cmap, alpha=heatmap.T)
 
-    def create_time_map(self, resolution, brightness, size_factor, sensitivity):
-        if self.positions:
-            x, y, timestamps = zip(*self.positions)
-
-            # Применяем чувствительность к временным меткам
-            adjusted_timestamps = np.array(timestamps) ** sensitivity
-
-            # Нормализация времени для цветовой карты
-            norm = Normalize(vmin=min(adjusted_timestamps), vmax=max(adjusted_timestamps))
-
-            if self.img is None:
-                self.img = np.zeros((800, 1200, 3), dtype=np.uint8)
-
-            self.ax.clear()  # Очистка оси
-
-            # Ограничиваем количество точек для отображения в зависимости от разрешения
-            total_points = len(x)
-            resolution_factor = int(np.clip(resolution, 1, 100))  # Ограничиваем разрешение от 1 до 100
-            num_points_to_show = int(total_points * (resolution_factor / 100))  # Количество точек зависит от разрешения
-
-            # Получаем выборку точек на основе разрешения
-            if num_points_to_show > 0:
-                indices = np.linspace(0, total_points - 1, num_points_to_show, dtype=int)
-                x = np.array(x)[indices]
-                y = np.array(y)[indices]
-                adjusted_timestamps = adjusted_timestamps[indices]
-
-            # Не переворачиваем ось Y для временной карты
-            # y = np.subtract(self.img.shape[0], y)  # УБИРАЕМ ЭТУ СТРОКУ
-
-            # Используем разрешение для изменения размера точек
-            sizes = np.full(len(x), 10 * size_factor)  # Размер точек будет зависеть от других параметров (size_factor)
-
-            # Отображаем точки
-            sc = self.ax.scatter(x, y,  # Оставляем точки как есть
-                                 c=adjusted_timestamps, cmap=self.color_map, norm=norm, s=sizes,
-                                 alpha=np.clip(brightness, 0, 1))
-
-            # Отображаем изображение
-            self.ax.imshow(np.flipud(self.img))  # Переворачиваем скриншот
-            self.ax.axis('off')
-            self.fig.tight_layout()
-
-            # Удаляем предыдущую цветовую шкалу, если она существует
-            if hasattr(self, 'cbar') and self.cbar:
-                self.cbar.remove()
-
-            # Не создаем новую цветовую шкалу в режиме временной карты
-            self.cbar = None
-
-            self.canvas.draw()
-
-    def update_map(self):
+    def update_heatmap(self):
         resolution = int(float(self.slider_resolution.get()))
         brightness = self.slider_brightness.get()
         size_factor = self.slider_size.get()
         sensitivity = self.slider_sensitivity.get()
 
-        self.ax.clear()  # Очистка оси нужна в обоих случаях
+        self.ax.clear()
+        if self.img is not None:
+            self.ax.imshow(np.flipud(self.img))
 
-        if self.is_time_map.get():
-            self.create_time_map(resolution, brightness, size_factor, sensitivity)  # Передаем все параметры
-        else:
-            if self.positions:
-                x, y, _ = zip(*self.positions)
-                x = np.array(x)
-                y = np.array(y)
-                self.create_heatmap(resolution, brightness, size_factor, sensitivity, self.img, x, y)
+        if self.positions:
+            if self.is_move_mode.get():
+                x_move, y_move = zip(*[(x, y) for x, y, is_click in self.positions if not is_click])
+                self.create_heatmap(resolution, brightness, size_factor, sensitivity, self.img,
+                                    (np.array(x_move), np.array(y_move)), self.color_map_moving)
+            if self.is_click_mode.get():
+                x_click, y_click = zip(*[(x, y) for x, y, is_click in self.positions if is_click])
+                self.create_heatmap(resolution, brightness, size_factor, sensitivity, self.img,
+                                    (np.array(x_click), np.array(y_click)), self.color_map_clicking)
 
+        self.ax.axis('off')
+        self.fig.tight_layout()
         self.canvas.draw()
+        self.root.update_idletasks()
 
     def save_heatmap(self):
         filepath = filedialog.asksaveasfilename(defaultextension=".png",
@@ -295,6 +262,7 @@ class MouseTrackerApp:
         self.stop_tracking()
         self.root.destroy()
         os._exit(0)
+
 
 if __name__ == "__main__":
     root = ttkb.Window()
